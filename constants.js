c98e109a8e44bb59bafb266f559bcda3db773909
MaksZhukov <maks_zhukov_97@mail.ru>
2020-08-08 12:58:51 +0300

    fixed extension urls

--- constants.js
+++ constants.js
@@ -1,1 +1,1 @@
-window.APP_URL = "https://e-translate.herokuapp.com/api/get-extension-key";
+window.APP_URL = 'https://e-translate.herokuapp.com/api';
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let toolTipRef = document.createElement("div");
+let toolTipRef = document.createElement('div');
 let x, y;
 let tt;
 
 let textLang, translateLang, etranslateExtensionKey;
 
 chrome.storage.sync.get(
-    ["textLang", "translateLang", "etranslateExtensionKey"],
+    ['textLang', 'translateLang', 'etranslateExtensionKey'],
     function (result) {
         textLang = result.textLang;
         translateLang = result.translateLang;
@@ -20,25 +20,25 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
         : translateLang;
 });
 
-toolTipRef.setAttribute("id", "etranslate-tooltip");
+toolTipRef.setAttribute('id', 'etranslate-tooltip');
 document.body.append(toolTipRef);
 
-document.addEventListener("mousedown", function (e) {
+document.addEventListener('mousedown', function (e) {
     x = e.clientX + window.scrollX;
     y = e.clientY + window.scrollY - 25;
 });
 
-document.addEventListener("mouseup", function (e) {
+document.addEventListener('mouseup', function (e) {
     let selection;
     if (window.getSelection) {
         selection = window.getSelection();
     } else if (document.selection) {
         selection = document.selection.createRange();
     }
-    if (selection.toString() !== "") {
-        toolTipRef.setAttribute("style", `left: ${x}px; top: ${y}px`);
-        let arrStr = selection.toString().split(" ");
-        if (arrStr.length < 11 && arrStr[0] !== "") {
+    if (selection.toString() !== '') {
+        toolTipRef.setAttribute('style', `left: ${x}px; top: ${y}px`);
+        let arrStr = selection.toString().split(' ');
+        if (arrStr.length < 11 && arrStr[0] !== '') {
             if (tt && !tt.popperInstance.popper.contains(e.target)) {
                 tt.dispose();
                 tt = null;
@@ -47,12 +47,12 @@ document.addEventListener("mouseup", function (e) {
                 fetch(
                     `${
                         window.APP_URL
-                    }?key=${etranslateExtensionKey}&textLang=${textLang}&translateLang=${translateLang}&text=${selection.toString()}`
+                    }/extension-translate?key=${etranslateExtensionKey}&textLang=${textLang}&translateLang=${translateLang}&text=${selection.toString()}`
                 )
                     .then((res) => res.json())
                     .then((text) => {
                         tt = new Tooltip(toolTipRef, {
-                            placement: "top",
+                            placement: 'top',
                             title: text,
                         }).show();
                     });
